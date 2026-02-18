64ea9258ffff4092bddf73d0837843cdd094b56d
Jonas Kernchen <kernchen.jonas@gmail.com>
2026-02-18 14:43:48 +0100

    food

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,73 @@
 
 */
 use bevy::{input::mouse::AccumulatedMouseScroll, prelude::*};
+use rand::{Rng, RngExt};
 
 // rs ant mesh
 #[derive(Resource)]
 pub struct AntMesh(pub Handle<Mesh>);
 
+// rs food mesh
+#[derive(Resource)]
+pub struct FoodMesh(pub Handle<Mesh>);
+
 // cp ant
 #[derive(Component)]
-pub struct Ant;
+pub struct Ant {
+    pub hunger: f32,
+}
+
+impl Default for Ant {
+    fn default() -> Self {
+        Self {
+            hunger: 10.
+        }
+    }
+}
+
+// fn spawn ant
+pub fn spawn_ant(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<ColorMaterial>>,
+    ant_mesh: &Res<AntMesh>,
+    transform: Transform,
+) {
+    commands.spawn((
+        Ant::default(),
+        Velocity::default(),
+        Mesh2d(ant_mesh.0.clone()),
+        MeshMaterial2d(materials.add(Color::srgb(0., 1., 0.))),
+
+        children![(
+            Mesh2d(meshes.add(Circle::new(100.0).to_ring(2.))),
+            MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+            Transform::default(),
+        )],
+    
+        transform,
+    ));
+}
+
+// fn spawn food
+pub fn spawn_food(
+    commands: &mut Commands,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<ColorMaterial>>,
+    food_mesh: &Res<FoodMesh>,
+    transform: Transform,
+) {
+    commands.spawn((
+        Food(100.),
+        Mesh2d(food_mesh.0.clone()),
+        MeshMaterial2d(materials.add(Color::srgb(1., 1., 0.))),
+        transform,
+    ));
+}
+
+// cp food
+#[derive(Component)]
+pub struct Food(pub f32);
 
 // cp velocity
 #[derive(Component, Default, Copy, Clone)]
@@ -26,8 +85,8 @@ pub struct Velocity {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(Update, (velocity_system, ant_movement, zoom_camera))
+        .add_systems(Startup, (setup, spawn_entities).chain())
+        .add_systems(Update, (velocity_system, ant_movement, zoom_system))
         .run()
     ;
 }
@@ -36,30 +95,52 @@ fn main() {
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2d);
-    let antmesh = meshes.add(Circle::new(10.0));
-    commands.insert_resource(AntMesh(antmesh.clone()));
-
-    commands.spawn((
-        Ant,
-        Velocity::default(),
-        Mesh2d(antmesh.clone()),
-        MeshMaterial2d(materials.add(Color::srgb(0., 1., 0.))),
-
-        children![(
-            Mesh2d(meshes.add(Circle::new(100.0).to_ring(2.))),
-            MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
-            Transform::default(),
-        )],
-        
-        Transform::default(),
-    ));
+    commands.insert_resource(AntMesh(meshes.add(Circle::new(10.0))));
+    commands.insert_resource(FoodMesh(meshes.add(Circle::new(20.0))));
 }
 
-// fn zoom camera
-fn zoom_camera(mouse_wheel: Res<AccumulatedMouseScroll>, camera_query: Single<&mut Projection, With<Camera>>){
+// fn spawn entities
+fn spawn_entities(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    ant_mesh: Res<AntMesh>,
+    food_mesh: Res<FoodMesh>,
+) {
+    let mut rng = rand::rng();
+
+    commands.spawn(Camera2d);
+
+    // spawn ants
+    for _ in 0..10 {
+        spawn_ant(
+            &mut commands,
+            &mut meshes,
+            &mut materials,
+            &ant_mesh,
+            Transform::default()
+        );
+    }
+
+    // spawn food
+    for _ in 0..200 {
+        spawn_food(
+            &mut commands,
+            &mut meshes,
+            &mut materials,
+            &food_mesh,
+            Transform::from_xyz(
+                rng.random_range(-10000.0..10000.0),
+                rng.random_range(-10000.0..10000.0),
+                0.
+            )
+        );
+    }
+}
+
+// fn zoom system
+fn zoom_system(mouse_wheel: Res<AccumulatedMouseScroll>, camera_query: Single<&mut Projection, With<Camera>>){
     match &mut *camera_query.into_inner() {
         Projection::Orthographic(orthographic) => {
             if mouse_wheel.delta.y < 0. {
@@ -77,8 +158,10 @@ fn zoom_camera(mouse_wheel: Res<AccumulatedMouseScroll>, camera_query: Single<&m
 fn ant_movement(
     ant_query: Query<&mut Velocity, With<Ant>>,
 ) {
+    let mut rng = rand::rng();
     for mut velocity in ant_query {
-        velocity.x += 1.;
+        velocity.x += rng.random_range(-1.0..1.0);
+        velocity.y += rng.random_range(-1.0..1.0);
     }
 }
 
